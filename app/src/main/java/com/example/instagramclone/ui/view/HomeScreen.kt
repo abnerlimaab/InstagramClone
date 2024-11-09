0f1879fbd92e238ab407a5fb2353ec7cbbbb913d
Abner <abnerlima.1997@gmail.com>
2024-11-09 07:04:45 -0300

    feat: add story list

--- app/src/main/java/com/example/instagramclone/ui/view/HomeScreen.kt
+++ app/src/main/java/com/example/instagramclone/ui/view/HomeScreen.kt
@@ -4,19 +4,61 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.instagramclone.data.Story
+import com.example.instagramclone.ui.theme.spacingMedium
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background).padding(top = 16.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(top = 16.dp)
+    ) {
         InstagramToolBar()
 
-        StoryItem(story = Story("janedoe", "https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"))
+        val stories = listOf(
+            Story(
+                "janedoe",
+                "https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            ),
+            Story(
+                "johnsmith",
+                "https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            ),
+            Story(
+                "emilybrown",
+                "https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            ),
+            Story(
+                "michaeljohnson",
+                "https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            ),
+            Story(
+                "sarawilson",
+                "https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            )
+        )
+
+        StoryList(stories = stories)
+    }
+}
+
+@Composable
+fun StoryList(stories: List<Story>) {
+    LazyRow(
+        modifier = Modifier.padding(top = spacingMedium)
+    ) {
+        itemsIndexed(stories) { _, item ->
+            StoryItem(story = item)
+        }
     }
 }
 
