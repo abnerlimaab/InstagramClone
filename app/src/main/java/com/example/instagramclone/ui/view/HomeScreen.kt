1d75386e5fbe6fc0b6d08c03b3ba0eeecf296712
Abner <abnerlima.1997@gmail.com>
2024-11-12 05:08:33 -0300

    feat: add feedlist

--- app/src/main/java/com/example/instagramclone/ui/view/HomeScreen.kt
+++ app/src/main/java/com/example/instagramclone/ui/view/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.HorizontalDivider
@@ -14,6 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.instagramclone.data.model.Feed
 import com.example.instagramclone.data.model.Story
+import com.example.instagramclone.data.repository.feedList
 import com.example.instagramclone.data.repository.stories
 import com.example.instagramclone.ui.theme.DividerColor
 import com.example.instagramclone.ui.theme.spacingMedium
@@ -32,16 +34,7 @@ fun HomeScreen() {
 
         HorizontalDivider(color = DividerColor, thickness = 0.2.dp)
 
-        FeedItem(
-            feed = Feed(
-                userNickName = "deassis",
-                localName = "Tiradentes - MG",
-                userAvatar = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Favatarfiles.alphacoders.com%2F216%2Fthumb-1920-216277.jpg&f=1&nofb=1&ipt=84259aa182480965fe70beae8ebcbd8baf38b4cdf133129fa71f03f7b8b33af6&ipo=images",
-                imageUrl = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp2861081.jpg&f=1&nofb=1&ipt=65713b41e1f29031a9b1380a4722f8a0ffcf19615e32f4d6303a814dc8ce4209&ipo=images",
-                description = "De quando aprendi a usar o chidore",
-                postedAgo = "Há 21 horas"
-            )
-        )
+        FeedList(feeds = feedList)
     }
 }
 
@@ -52,6 +45,17 @@ fun StoryList(stories: List<Story>) {
     ) {
         itemsIndexed(stories) { _, item ->
             StoryItem(story = item)
+        }
+    }
+}
+
+@Composable
+fun FeedList(feeds: List<Feed>) {
+    LazyColumn (
+        modifier = Modifier.padding(top = spacingMedium)
+    ) {
+        itemsIndexed(feeds) { _, item ->
+            FeedItem(feed = item)
         }
     }
 }
